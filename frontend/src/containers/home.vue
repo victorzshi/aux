b14f3005d6f353b2673cead2ae97ce4e36946182
VictoryShoe <victoryshoe@hotmail.com>
2017-10-21 16:14:03 -0400

    Complete landing page layout

--- frontend/src/containers/home.vue
+++ frontend/src/containers/home.vue
@@ -1,9 +1,171 @@
 <template>
-<div>Home</div>
+<section class="hero is-dark is-bold is-fullheight">
+  <!-- Hero head: will stick at the top -->
+  <div class="hero-head">
+    <div class="container">
+      <nav class="level">
+        <!-- Left side -->
+        <div class="level-left">
+          <div class="level-item">
+            <a href="#"><h1 class="title">Aux</h1></a>
+          </div>
+        </div>
+        <!-- Right side -->
+        <div class="level-right">
+          <div class="level-item">
+            <a href="#"><h2 class="subtitle">About</h2></a>
+          </div>
+        </div>
+      </nav>
+      
+    </div>
+  </div>
+
+  <!-- Hero content: will be in the middle -->
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <div class="columns is-centered">
+
+        <!-- Start of buttons -->
+        <div id="buttons">
+          <div class="column is-centered is-narrow">
+
+            <!-- Create room button -->
+            <div class="column">
+              <a class="button is-dark is-inverted is-large">
+                <!-- For icon -->
+                <!-- <span class="icon is-medium">
+                  <i class="fa fa-github"></i>
+                </span> -->
+                <span>Create</span>
+              </a>
+            </div>
+
+            <!-- Text field to enter room code -->
+            <div id="room_entry_field" class="columns is-centered">
+              <div class="column is-two-thirds field">
+                <div class="control">
+                  <input class="input is-medium has-text-centered" type="text" placeholder="Room Code">
+                </div>
+              </div>
+            </div>
+
+            <!-- Join existing room submit button -->
+            <div class="column">
+              <a class="button is-dark is-inverted is-large">
+                <!-- For icon -->
+                <!-- <span class="icon is-medium">
+                  <i class="fa fa-github"></i>
+                </span> -->
+                <span>Join</span>
+              </a>
+            </div>
+            
+          </div>
+        </div>
+        <!-- End of buttons -->
+
+        <!-- Start of visuals -->
+        <div class="column is-half">
+          <div class="column has-text-centered">
+            <h2 class="subtitle">
+              Popular rooms right now
+            </h2>
+          </div>
+
+          <!-- Pictures first row-->
+          <div class="columns">
+            <div class="column">
+              <figure class="image is-1by1">
+                <img src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image is-1by1">
+                <img src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image is-1by1">
+                <img src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+          </div>
+
+          <!-- Pictures second row-->
+          <div class="columns">
+            <div class="column">
+              <figure class="image is-1by1">
+                <img src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image is-1by1">
+                <img src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+            <div class="column">
+              <figure class="image is-1by1">
+                <img src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+          </div>
+
+        </div>
+        <!-- End of visuals -->
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Hero footer: will stick at the bottom -->
+  <div class="hero-foot">
+    <div class="container">
+      <nav class="level">
+
+        <!-- Left side -->
+        <div class="level-left">
+          <div class="content">
+            <p>Made for Basement Hacks 2017</p>
+          </div>
+        </div>
+
+        <div class="level-item">
+          <div class="content">
+            <p>Created by Andrew McCann, Sangeet Parashar, Jerry Yuan Chen, and Victor Shi</p>
+          </div>
+        </div>
+
+
+        <!-- Right side -->
+        <div class="level-right">
+          <a href="https://bulma.io">
+            <img src="../assets/made-with-bulma--white.png" alt="Made with Bulma" width="128" height="24">
+          </a>
+        </div>
+
+      </nav>
+    </div>
+  </div>
+</section>
 </template>
 
 <style scoped>
-  
+
+.hero-head {
+  margin-top: 2.5%;
+}
+
+#buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#room_entry_field {
+  margin-top: 15%;
+}
+
 </style>
 
 <script>
