8db24213f443d8c60226aad89ac1ea18634dbbb0
Andrew <amccan5@uwo.ca>
2017-10-21 23:57:20 -0400

    oauth2 working

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,22 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var SpotifyWebApi = require('spotify-web-api-node');
+var session = require('express-session'),
+	passport = require('passport'),
+	SpotifyStrategy = require('passport-spotify').Strategy;
 
 /**
  * Set the credentials given on Spotify's My Applications page.
  * https://developer.spotify.com/my-applications
  */
-var client_id = '2421a4b55e5c457eb279cacbacb9d318'; // Your client id
-var client_secret = '97d2682f52c44db6ab68a812d72e59c0'; // Your secret
-var redirect_uri = 'http://localhost:3000/users/callback'; // Your redirect uri
-spotifyApi = new SpotifyWebApi({
-	clientId : client_id,
-	clientSecret : client_secret,
-	redirectUri : redirect_uri
-});
+// var client_id = '2421a4b55e5c457eb279cacbacb9d318'; // Your client id
+// var client_secret = '97d2682f52c44db6ab68a812d72e59c0'; // Your secret
+// var redirect_uri = 'http://localhost:3000/users/callback'; // Your redirect uri
+// spotifyApi = new SpotifyWebApi({
+// 	clientId : client_id,
+// 	clientSecret : client_secret,
+// 	redirectUri : redirect_uri
+// });
 
 // Get models.
 var mongoose = require('mongoose'); //add for Mongo support
@@ -43,6 +46,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'keyboard cat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Redirect routes to the controllers.
 app.use('/', index);
